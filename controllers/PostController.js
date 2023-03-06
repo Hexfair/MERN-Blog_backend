b2fc1fdaf1c98041c5a348cfed74a003cac8b0a5
Hexfair <hexfair17@gmail.com>
2023-03-06 17:44:32 -0500

    first commit

---
+++ controllers/PostController.js
@@ -0,0 +1,113 @@
+import PostModel from '../models/Post.js';
+//=========================================================================================================================
+
+export const getAll = async (req, res) => {
+	try {
+		const posts = await PostModel.find().populate('user').exec();
+
+		res.json(posts);
+
+	} catch (err) {
+		console.log(err);
+		res.status(400).json({ message: 'Ошибка при получении статей' });
+	}
+};
+
+export const getOne = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		PostModel.findOneAndUpdate(
+			{
+				_id: postId,
+			},
+			{
+				$inc: { viewsCount: 1 },
+			},
+			{
+				returnDocument: 'after',
+			},
+			(err, doc) => {
+				if (err) {
+					return res.status(500).json({ message: 'Не удалось получить статью' });
+				};
+
+				if (!doc) {
+					return res.status(404).json({ message: 'Статья не найдена' });
+				};
+
+				res.json(doc);
+			}
+		);
+
+	} catch (err) {
+		console.log(err);
+		res.status(400).json({ message: 'Ошибка при получении статьи' });
+	}
+};
+
+export const create = async (req, res) => {
+	try {
+		const doc = new PostModel({
+			title: req.body.title,
+			text: req.body.text,
+			tags: req.body.tags,
+			imageUrl: req.body.imageUrl,
+			user: req.userId,
+		});
+
+		const post = await doc.save();
+
+		res.json(post);
+
+	} catch (err) {
+		console.log(err);
+		res.status(400).json({ message: 'Ошибка при создании статьи' });
+	}
+};
+
+export const remove = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		PostModel.findByIdAndDelete(
+			{
+				_id: postId,
+			},
+			(err, doc) => {
+				if (err) {
+					return res.status(500).json({ message: 'Не удалось получить статью' });
+				};
+
+				if (!doc) {
+					return res.status(404).json({ message: 'Статья не найдена' });
+				};
+
+				res.json({ message: 'Статья успешно удалена!' });
+			}
+		)
+	} catch (err) {
+		console.log(err);
+		res.status(400).json({ message: 'Ошибка при удалении статьи' });
+	}
+};
+
+export const update = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		await PostModel.updateOne(
+			{
+				_id: postId,
+			},
+			{
+				title: req.body.title,
+				text: req.body.text,
+				imageUrl: req.body.imageUrl,
+				user: req.userId,
+				tags: req.body.tags
+			},
+		)
+		res.json({ message: 'Статья успешно обновлена!' });
+	} catch (err) {
+		console.log(err);
+		res.status(400).json({ message: 'Ошибка при обновлении статьи' });
+	}
+}
---
+++ index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import { registerValidator, loginValidator, postCreateValidator } from './utils/validations.js';
+import checkAuth from './utils/checkAuth.js';
+import handleValidationErrors from './utils/handleValidationErrors.js';
+import { UserController, PostController } from './controllers/index.js';
+//=========================================================================================================================
+
+mongoose
+	.set("strictQuery", true)
+	.connect('mongodb+srv://admin:admin123@cluster0.iadlc0k.mongodb.net/blog?retryWrites=true&w=majority')
+	.then(() => console.log('Подключено к MongoDB!'))
+	.catch((err) => console.log('Ошибка подключения к MongoDB!', err));
+
+
+/* Middleware */
+const app = express();
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+/* Storage */
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		cb(null, 'uploads');
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname);
+	},
+});
+const upload = multer({ storage });
+
+
+/* Routes */
+// User routes
+app.post('/auth/register', registerValidator, handleValidationErrors, UserController.register);
+app.post('/auth/login', loginValidator, handleValidationErrors, UserController.login);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+// Post routes
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidator, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidator, handleValidationErrors, PostController.update);
+
+// Upload route
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+	res.json({
+		url: `/uploads/${req.file.originalname}`,
+	})
+});
+
+
+app.listen(4444, (err) => {
+	if (err) console.log('Ошибка запуска локального сервера!');
+	console.log('Локальный сервер запущен!');
+});
